c75dfe541779a41a7303e5b24c4b489ba0ecb132
xingct34-pixel <xingct34@gmail.com>
2026-09-15 20:03:59 +0800

    Update thread_safe_queue.h

--- thread_safe_queue.h
+++ thread_safe_queue.h
@@ -12,7 +12,7 @@ public:
         std::unique_lock<std::mutex> lock(mutex_);
 
         //第一个问题，no_full是条件变量，给生产者线程用，执行逻辑：先运行lambda，拿到返回值(true/false)，返回 false → 生产者线程阻塞休眠，释放互斥锁，
-          让消费者可以拿到锁，等待被再次唤醒。如果被唤醒，拿到互斥锁，再次执行lamda判断，以防虚假唤醒；返回 true → 不阻塞，生产者继续生产，
+         // 让消费者可以拿到锁，等待被再次唤醒。如果被唤醒，拿到互斥锁，再次执行lamda判断，以防虚假唤醒；返回 true → 不阻塞，生产者继续生产，
         not_full_.wait(lock, [this] { return queue_.size() < max_size_; });
         queue_.push(std::move(item));
         lock.unlock();
