07ddfe54db5fcc217deb30d3ac51b2cfdcca69d3
xingct34-pixel <xingct34@gmail.com>
2026-09-15 18:50:33 +0800

    Update thread_safe_queue.h

--- thread_safe_queue.h
+++ thread_safe_queue.h
@@ -10,6 +10,9 @@ public:
 
     void push(T item) {
         std::unique_lock<std::mutex> lock(mutex_);
+
+        //第一个问题，no_full是条件变量，给生产者线程用，执行逻辑：先运行lambda，拿到返回值(true/false)，返回 false → 生产者线程阻塞休眠，释放互斥锁，
+          让消费者可以拿到锁，等待被再次唤醒。如果被唤醒，拿到互斥锁，再次执行lamda判断，以防虚假唤醒；返回 true → 不阻塞，生产者继续生产，
         not_full_.wait(lock, [this] { return queue_.size() < max_size_; });
         queue_.push(std::move(item));
         lock.unlock();
@@ -19,7 +22,7 @@ public:
     T pop() {
         std::unique_lock<std::mutex> lock(mutex_);
         not_empty_.wait(lock, [this] { return !queue_.empty(); });
-        T item = std::move(queue_.front());
+        T item = std::move(queue_.front());           //queue_front队列第一个元素
         queue_.pop();
         lock.unlock();
         not_full_.notify_one();
